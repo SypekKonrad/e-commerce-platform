a8403ec97bc6992eed58202c3c41a565f9846872
Konrad Sypek <konrad.sypek@o2.pl>
2024-10-03 23:02:34 +0200

    settings.py/installed_apps refactor

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,13 +31,20 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 # Application definition
 
 INSTALLED_APPS = [
+
+    # built-in
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
     'rest_framework',
+
+    # my apps
+    'home',
 ]
 
 MIDDLEWARE = [
